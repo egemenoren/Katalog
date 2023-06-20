c80dd144a414a0348825835bb74c3f433afe180d
Egemen Ören <egemenorenn@gmail.com>
2023-06-20 17:55:16 +0300

    ResponseDto sharing has been all set.

--- src/Services/Product/Katalog.Product/Controllers/BrandsController.cs
+++ src/Services/Product/Katalog.Product/Controllers/BrandsController.cs
@@ -1,4 +1,5 @@
 ﻿using Katalog.Product.Repositories.Abstract;
+using Katalog.Shared.Dtos;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
 
@@ -21,27 +22,27 @@ namespace Katalog.Product.Controllers
         #region Create
         [HttpPost("create")]
         [ProducesResponseType(typeof(Entities.Brand), (int)HttpStatusCode.Created)]
-        public async Task<ActionResult<Entities.Brand>> CreateBrand([FromBody] Entities.Brand brand)
+        public async Task<ActionResult<ResponseDto<Entities.Brand>>> CreateBrand([FromBody] Entities.Brand brand)
         {
             await _brandRepository.Create(brand);
-            return CreatedAtRoute("GetBrand", new { id = brand.Id }, brand);
+            return CreatedAtRoute("GetBrand", new { id = brand.Id }, ResponseDto<Entities.Brand>.Success(brand,200));
         }
         #endregion
 
         #region Get All
         [HttpGet("getallbrands")]
-        [ProducesResponseType(typeof(Entities.Brand), (int)HttpStatusCode.OK)]
-        public async Task<ActionResult<IEnumerable<Entities.Brand>>> GetBrands()
+        [ProducesResponseType(typeof(ResponseDto<IEnumerable<Entities.Brand>>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ResponseDto<IEnumerable<Entities.Brand>>>> GetBrands()
         {
             var brands = await _brandRepository.GetAll();
-            return Ok(brands);
+            return Ok(ResponseDto<IEnumerable<Entities.Brand>>.Success(brands,200));
         }
         #endregion
         
         #region Get By Id
         [HttpGet("{id:length(24)}", Name = "GetBrand")]
         [ProducesResponseType((int)HttpStatusCode.NotFound)]
-        [ProducesResponseType(typeof(Entities.Brand), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ResponseDto<Entities.Brand>), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<Entities.Brand>> GetBrand(string id)
         {
             var brand = await _brandRepository.GetById(id);
@@ -49,16 +50,25 @@ namespace Katalog.Product.Controllers
             {
                 return NotFound();
             }
-            return Ok(brand);
+            return Ok(ResponseDto<Entities.Brand>.Success(brand,200));
         }
         #endregion
 
         #region Update
         [HttpPut("update")]
-        [ProducesResponseType(typeof(Entities.Brand), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ResponseDto<Entities.Brand>), (int)HttpStatusCode.OK)]
         public async Task<IActionResult> UpdateBrand([FromBody] Entities.Brand brand)
         {
-            return Ok(await _brandRepository.Update(brand));
+            try
+            {
+            await _brandRepository.Update(brand);
+            return Ok(ResponseDto.Success(200));
+                
+            }
+            catch (Exception ex)
+            {
+                return Ok(ResponseDto.Fail(ex.Message,500));
+            }
         }
         #endregion
 
@@ -67,22 +77,25 @@ namespace Katalog.Product.Controllers
         [ProducesResponseType(typeof(Entities.Brand), (int)HttpStatusCode.OK)]
         public async Task<IActionResult> UpdateMany([FromBody] List<Entities.Brand> brands)
         {
-            return Ok(await _brandRepository.UpdateMany(brands));
+            var result = await _brandRepository.UpdateMany(brands);
+            if (result)
+                return Ok(ResponseDto.Success(200));
+            return Ok(ResponseDto<Entities.Brand>.Fail("An unexpeced error occurred",500));
         }
         #endregion
 
         #region Delete
         [HttpDelete("delete")]
-        [ProducesResponseType(typeof(Entities.Brand), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ResponseDto<bool>), (int)HttpStatusCode.OK)]
         public async Task<IActionResult> DeleteBrandById(string id)
         {
-            return Ok(await _brandRepository.Delete(id));
+            return Ok(ResponseDto<bool>.Success(await _brandRepository.Delete(id),200));
         }
         #endregion
 
         #region Bulk Delete
         [HttpDelete("deletebulk")]
-        [ProducesResponseType(typeof(Entities.Brand), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ResponseDto<bool>), (int)HttpStatusCode.OK)]
         public async Task<IActionResult> DeleteMany(List<string> ids)
         {
             return Ok(await _brandRepository.DeleteMany(ids));
